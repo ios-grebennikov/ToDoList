7801891a01fe0412f8d851a4fe1d000959c6b004
Alexey Grebennikov <ios.grebennikov@gmail.com>
2022-03-05 20:51:16 +0300

    Initial commit

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Alexey Grebennikov on 5.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
